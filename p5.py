abf3bbf3f58f0aae4ef0d9e569d266a93fe66b85
nimamjd <99634197+nimamjd@users.noreply.github.com>
2022-03-30 19:00:55 +0430

    Add files via upload

--- p5.py
+++ p5.py
@@ -1,9 +1,14 @@
-n = 1
-factor = 1
-while factor <= n:
-    if n % factor == 0:
-        print(factor)
-        factor += 1
-        print()
-        n += 1
-        
+for i in range(1, 10001):
+    t = i
+    s = 0
+    c = 0
+    while t != 0:
+        c += 1
+        t //= 10
+    t = i
+    while t != 0:
+        r = t % 10
+        s += r ** c
+        t //= 10
+    if i == s:
+        print(i)
